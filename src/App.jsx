9a64e5f70b93d89d62723c1ae4fa22d7ec397c21
datta504 <dattasaiyadav50@gmail.com>
2025-04-30 04:14:00 +0000

    Lab6

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
-import productData from './data/full-products';
+import productData from './data/full-products';	import productData from './data/full-products';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello There.</h1>
-    </div>
-  );
-}
 
-export default App;
+
+
+function App() {	function App() {
+  return (	  
+     (
+    <div className="App">	    <div className="App">
+      <h1>Hello There.</h1>	       <Header />
+      <Routes>
+        <Route path="/" element={<CardList data={productData} />} />
+        <Route path="/product/:id" element={<SingleView data={productData} />} />
+      </Routes>
+    </div>	    </div>
+  );	  ;
+}	}
+
+
+export default App;	export default App;
+
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from 'react'	import React from 'react'
 
-export default function Button({text, handleClick}) {
-  return (
-    <a href="#" className="f5 no-underline black bg-animate hover-bg-black hover-white inline-flex items-center pa3 ba border-box mr4" onClick={handleClick}>
-      <span className="pl1">{text}</span>
-    </a>
-  )
-}
+
+export default function Button({text, handleClick}) {	const Button = ({ text, handleClick, disabled }) => {
+  return (	  return 
+    <a href="#" className="f5 no-underline black bg-animate hover-bg-black hover-white inline-flex items-center pa3 ba border-box mr4" onClick={handleClick}>	    <button 
+      <span className="pl1">{text}</span>	    onClick={handleClick}
+    </a>	    disabled={disabled}
+  )	    className={`f6 link dim br3 ph3 pv2 mb2 dib white bg-black ma2 ${disabled ? 'opacity-50 cursor-not-allowed' : ''}`}
+}	  >
+    {text}
+  </button>
+);
+};
+
+export default Button;
